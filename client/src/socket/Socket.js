adeead580ea8d6f2c3924ae60a69399bdc105289
Shivani <shivanitripurari07@gmail.com>
2025-05-31 17:57:31 +0530

    updated with backend endpoint

--- client/src/socket/Socket.js
+++ client/src/socket/Socket.js
@@ -1,2 +1,4 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:3001');
+// export const socket = io('http://localhost:3001');
+export const socket = io('http://live-polling-system.up.railway.app');
+
