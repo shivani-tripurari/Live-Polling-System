ce063dcde4e9c8a54d6a302aa713fed6d932ccc2
Shivani <shivanitripurari07@gmail.com>
2025-05-31 18:15:20 +0530

    Resolved mixed content error

--- client/src/socket/Socket.js
+++ client/src/socket/Socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 // export const socket = io('http://localhost:3001');
-export const socket = io('http://live-polling-system.up.railway.app');
+export const socket = io('https://live-polling-system.up.railway.app');
 
